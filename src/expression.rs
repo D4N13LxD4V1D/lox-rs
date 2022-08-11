47083cb390a8772646e38967a6aacb11d90b2a4a
Daniel David Bador <danieldavidbador@yahoo.com>
2022-08-12 03:07:47 +0800

    removed expression.rs due to redundancy

--- src/expression.rs
+++
@@ -1,9 +0,0 @@
-use crate::token::Token;
-
-#[derive(Debug, Clone)]
-pub enum Expression {
-    Literal(Token),
-    Unary(Token, Box<Expression>),
-    Binary(Box<Expression>, Token, Box<Expression>),
-    Grouping(Box<Expression>),
-}
