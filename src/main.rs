eb8fcfe9f906d499b743a66a68e3bf0a3654cf46
Daniel David Bador <danieldavidbador@yahoo.com>
2022-08-11 20:08:19 +0800

    implemented basic init

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
+#![warn(clippy::all, clippy::pedantic)]
+use std::env;
+use std::fs;
+use std::io::Write;
+
+pub mod error_handling;
+pub mod scanner;
+pub mod token;
+
+fn run(source: &str) {
+    let tokens = scanner::scan(source);
+    println!("{:#?}", &tokens);
+}
+
+fn run_file(args: Vec<String>) {
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
+    for line in contents.split_terminator('\n') {
+        run(&line);
+    }
+}
+
+fn run_prompt() {
+    loop {
+        let mut input = String::new();
+        print!("\x1b[1;37m>>> \x1b[0m");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        run(&input);
+    }
+}
 
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        run_file(args);
+    } else {
+        run_prompt();
+    }
 }
