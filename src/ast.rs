9876377daabd932df67b2a576e0a1170771830f1
Daniel David Bador <danieldavidbador@yahoo.com>
2022-08-12 03:07:32 +0800

    added statements

--- src/ast.rs
+++ src/ast.rs
@@ -1,24 +1,56 @@
 use std::io::{self, Write};
 
-use crate::expression::Expression;
 use crate::token::{TokenType::{*, self}, Token};
 use crate::error_handling;
+
+#[derive(Debug, Clone)]
+pub enum Expression {
+    Literal(Token),
+    Unary(Token, Box<Expression>),
+    Binary(Box<Expression>, Token, Box<Expression>),
+    Grouping(Box<Expression>),
+    Variable(Token)
+}
+
+#[derive(Debug, Clone)]
+pub enum Statement {
+    Expression(Expression),
+    Print(Expression),
+    Var(Token, Expression)
+}
+
 struct State {
     tokens: Vec<Token>,
     current: usize,
 }
 
-pub fn print(expr: &Expression) {
+pub fn print(statements: &Vec<Statement>) {
+    for stmt in statements {
+        match stmt {
+            Statement::Print(e) => {
+                _print(&Expression::Unary(Token::new(PRINT, "print".to_string(), vec!['p', 'r', 'i', 'n', 't'], 0, 1), Box::from(e.clone())));
+            },
+            Statement::Expression(e) => {
+                _print(e);
+            },
+            Statement::Var(t, e) => {
+                _print(&Expression::Binary(Box::from(Expression::Variable(t.clone())), Token::new(VAR, "=".to_string(), vec!['='], 0, 1), Box::from(e.clone())));
+            }
+        }
+    }
+}
+
+fn _print(expr: &Expression) {
     match expr {
         Expression::Binary(left, op, right) => {
             print!("\x1b[1;34m( \x1b[0m{} ", op.lexeme); io::stdout().flush().unwrap();
-            print(&*left); io::stdout().flush().unwrap();
-            print(&*right); io::stdout().flush().unwrap();
+            _print(&*left); io::stdout().flush().unwrap();
+            _print(&*right); io::stdout().flush().unwrap();
             print!("\x1b[1;34m)\x1b[0m "); io::stdout().flush().unwrap();
         },
         Expression::Grouping(expr) => {
             print!(" \x1b[1;35m( \x1b[0m"); io::stdout().flush().unwrap();
-            print(&*expr); io::stdout().flush().unwrap();
+            _print(&*expr); io::stdout().flush().unwrap();
             print!("\x1b[1;35m)\x1b[0m "); io::stdout().flush().unwrap();
         },
         Expression::Literal(value) => {
@@ -26,19 +58,68 @@ pub fn print(expr: &Expression) {
         },
         Expression::Unary(op, expr) => {
             print!(" \x1b[1;33m( \x1b[0m{} ", op.lexeme); io::stdout().flush().unwrap();
-            print(&*expr); io::stdout().flush().unwrap();
+            _print(&*expr); io::stdout().flush().unwrap();
             print!("\x1b[1;33m)\x1b[0m "); io::stdout().flush().unwrap();
+        },
+        Expression::Variable(value) => {
+            print!("{} ", value.lexeme); io::stdout().flush().unwrap();
         }
     }
 }
 
-pub fn parse(tokens: Vec<Token>) -> Expression {
+pub fn parse(tokens: Vec<Token>) -> Vec<Statement> {
     let mut state = State {
         tokens,
         current: 0,
     };
+
+    let mut statements: Vec<Statement> = Vec::new();
+
+    while !is_at_end(&mut state) {
+        statements.push(declaration(&mut state));
+    }
     
-    expression(&mut state)
+    statements
+}
+
+fn declaration(state: &mut State) -> Statement {
+    if _match(state, vec![VAR]) {
+        return var_declaration(state);
+    }
+
+    statement(state)
+}
+
+fn var_declaration(state: &mut State) -> Statement {
+    let name = consume(state, IDENTIFIER, "variable");
+    
+    let initializer;
+    if _match(state, vec![EQUAL]) {
+        initializer = expression(state);
+    } else {
+        initializer = Expression::Literal(Token::new(NIL, "nil".to_string(), vec!['n', 'i', 'l'], 0, 1));
+    }
+    consume(state, SEMICOLON, ";");
+    Statement::Var(name, initializer)
+}
+
+fn statement(state: &mut State) -> Statement {
+    if _match(state, vec![PRINT]) {
+        return print_stmt(state);
+    }
+    expr_stmt(state)
+}
+
+fn print_stmt(state: &mut State) -> Statement {
+    let value = expression(state);
+    consume(state, SEMICOLON, ";");
+    Statement::Print(value)
+}
+
+fn expr_stmt(state: &mut State) -> Statement {
+    let expr = expression(state);
+    consume(state, SEMICOLON, ";");
+    Statement::Expression(expr)
 }
 
 fn expression(state: &mut State) -> Expression {
@@ -125,12 +206,12 @@ fn primary(state: &mut State) -> Expression {
     }
     
     if _match(state, vec![IDENTIFIER]) {
-        return Expression::Literal(previous(state));
+        return Expression::Variable(previous(state));
     }
     
     if _match(state, vec![LEFT_PAREN]) {
         let expr = expression(state);
-        consume(state, RIGHT_PAREN, ')');
+        consume(state, RIGHT_PAREN, ")");
         return Expression::Grouping(Box::new(expr));
     }
     let token = &state.tokens[state.current];
@@ -138,12 +219,12 @@ fn primary(state: &mut State) -> Expression {
     Expression::Literal(Token::new(NIL, "".to_string(), vec![], token.line, token.index))
 }
 
-fn consume(state: &mut State, token_type: TokenType, literal: char) -> Token {
+fn consume(state: &mut State, token_type: TokenType, expected: &str) -> Token {
     if check(state, token_type) {
         advance(state)
     } else {
         let token = &state.tokens[state.current];
-        error_handling::error(token.line, token.index, &("expecting '".to_owned() + &literal.to_string() + "'"));
+        error_handling::error(token.line, token.index, &("expecting '".to_owned() + expected + "'"));
         advance(state)
     }
 }
